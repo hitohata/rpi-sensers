9a96f6bbe5a4a1ea6dd226582931f5d52e6016e4
hirohata <hirohatatro@gmail.com>
2023-06-20 08:10:20 +0100

    add LED

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use dht11::DHT11;
 use rppal::hal::Delay;
+use led_light::LEDPin;
 
 fn main() {
 
@@ -7,12 +8,15 @@ fn main() {
     let temp_sensor_led = 6;
 
     let mut dht11 = DHT11::new(temperature_sensor_pin).unwrap();
+    let mut dht11_led = LEDPin::new(temp_sensor_led).unwrap();
 
     let handle = std::thread::spawn(move || {
         let mut delay = Delay::new();
 
         loop {
+            dht11_led.turn_on();
             println!("{:?}", dht11.read(&mut delay));
+            dht11_led.turn_off();
             std::thread::sleep(std::time::Duration::from_secs(10));
         }
 

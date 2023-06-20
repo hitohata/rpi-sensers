f3d97dc6d67f0aa032118a94df0c8860b8f534af
hirohata <hirohatatro@gmail.com>
2023-06-20 09:35:38 +0100

    add error led

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use dht11::DHT11;
 use rppal::hal::Delay;
+use tokio::sync::mpsc;
 use led_light::LEDPin;
+use std::sync::mpsc::{ Receiver };
 
 #[tokio::main]
 async fn main() {
@@ -11,21 +13,51 @@ async fn main() {
     let mut dht11 = DHT11::new(temperature_sensor_pin).unwrap();
     let mut dht11_led = LEDPin::new(temp_sensor_led).unwrap();
 
+    let (tx, mut rx) = std::sync::mpsc::channel();
+
     let handle = std::thread::spawn(move || {
         let mut delay = Delay::new();
 
+        let mut errored = false;
+
         loop {
             dht11_led.turn_on();
-            println!("{:?}", dht11.read(&mut delay));
+            match dht11.read(&mut delay) {
+                Ok(temperature) => { 
+                    println!("{:?}", temperature);
+                    if errored {
+                        tx.send(true).unwrap();
+                    }
+                },
+                Err(e) => {
+                    println!("{:?}", e);
+                    tx.send(false).unwrap();
+                    errored = true;
+                }
+            }
             dht11_led.turn_off();
-            std::thread::sleep(std::time::Duration::from_secs(10));
+            std::thread::sleep(std::time::Duration::from_secs(3));
         }
 
     });
 
+    let error_light = std::thread::spawn(move || {
+        error_light(rx);
+    });
+
     handle.join().unwrap();
+    error_light.join().unwrap();
 }
 
-fn error_light() {
-    let 
+fn error_light(rx: Receiver<bool>) {
+    let mut error_light = LEDPin::new(13).unwrap();
+
+    while let Ok(state) = rx.recv() {
+        if state {
+            error_light.turn_off();
+        } else {
+            error_light.turn_on();
+        }
+    }
+    
 }
